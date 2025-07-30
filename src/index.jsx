9c898572e6ace0d56a9e30a47636ab4716bb717c
Trey Holt <trey@tlcsampling.com>
2025-07-30 00:02:19 -0500

    chore: rename entry files to .jsx so Vite transforms JSX

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import './index.css';
 
 // Entry point for the React application. It attaches the root

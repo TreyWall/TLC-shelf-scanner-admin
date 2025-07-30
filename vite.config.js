32368d5d28a849f9c4ef2751dcaf1d469a333a74
Trey Holt <trey@tlcsampling.com>
2025-07-30 00:12:07 -0500

    fix: enable JSX in .js files via Vite include pattern

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
-        react()    // use the plugin with its defaults (handles .js, .jsx, .ts, .tsx)
+        react({
+            // transform JSX in .js, .jsx, .ts, .tsx
+            include: "**/*.{js,jsx,ts,tsx}"
+        })
     ]
 });
